bf74c0b364236c56f8ccce69d2212d598cc95850
eliasu <elias.ulen@gmail.com>
2019-11-24 00:32:58 +0100

    Shared media component

--- src/app/components/media/media.component.ts
+++ src/app/components/media/media.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Media, Playback } from '../../data/internal/media';
+import { MediaAdministratorService } from '../../services/media/media-administrator.service';
 
 @Component({
   selector: 'app-media',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaComponent implements OnInit {
 
-  constructor() { }
+  @Input() media: Media;
+
+  constructor(public mediaAdministratorService: MediaAdministratorService) { }
+
+  showPlayback(playback: Playback, media: Media) : boolean
+  {
+    return false;
+  }
+
+  showMedia(media: Media) : boolean
+  {
+    return false;
+  }
+
+  getVideoId(media: Media) : string
+  {
+    return null;
+  }
+
+  isMp4(playback: Playback) : boolean
+  {
+    return true;
+  }
+
+  isHls(playback: Playback) : boolean
+  {
+    return false;
+  }
+
+  isEmbed(playback: Playback) : boolean
+  {
+    return false;
+  }
 
   ngOnInit() {
+    console.log(this.media)
   }
 
 }
