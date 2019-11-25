d0b4e66ec3ff50f90cceb1e2519b02ee290edcca
eliasu <elias.ulen@gmail.com>
2019-11-25 20:40:52 +0100

    env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { SpinnerService } from './services/spinner/spinner.service';
 import { DataProxyService } from './proxies/data-proxy/data-proxy.service';
+import { environment } from '../environments/environment'
+
 
 @Component({
   selector: 'app-root',
@@ -21,6 +23,9 @@ export class AppComponent {
 
      // the lang to use, if the lang isn't available, it will use the current loader to get them
     translateService.use('en');
+
+    console.log('env')
+    console.log(environment.apiUrl)
 
     dataProxyService.poke().subscribe(x => {
         console.log('poke completed')
