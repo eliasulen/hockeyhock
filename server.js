b02b37afe454f82f1680131d03dc4c43e1feeffa
eliasu <elias.ulen@gmail.com>
2020-08-18 11:13:51 +0200

    Redirect custom domain

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Http -> Https redirect
+var appName = 'hockeyhock'
+var customHost = 'https://hh.eliasulen.se' //Empty if none
+var customHostProtocol = 'https://'
+var standard = 'herokuapp'
+
+// Http -> Https redirect and custom domain redirect
 app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`)
-  } else {
+  var host = customHost ? customHost : req.header('host')
+
+  if(customHost && req.header('host').includes(standard))
+  {
+    res.redirect(`${customHostProtocol}${host}${req.url}`) //Standard -> 
+  }
+  else if (req.header('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${host}${req.url}`) //Http -> Https
+  } 
+   else {
     next();
   }
 });
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/hockeyhock'));
+app.use(express.static(__dirname + `/dist/${appName}`));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/hockeyhock/index.html'));
+  res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`));
 });
 
 // default Heroku PORT
